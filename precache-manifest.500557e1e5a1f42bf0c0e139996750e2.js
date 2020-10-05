61715667a91d7cf82cff1e6c8ac299c3e96f887e
zaheralhaleeb <zalhaleb@gmail.com>
2020-10-05 15:12:00 +0200

    Updates

---
+++ precache-manifest.500557e1e5a1f42bf0c0e139996750e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4824ea08195b2934691401c2e9a316b",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "63accf6bcd464184eda8",
+    "url": "/react-project/static/css/main.f1926342.chunk.css"
+  },
+  {
+    "revision": "5d9c1e85f8394a3a7d1e",
+    "url": "/react-project/static/js/2.76520746.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-project/static/js/2.76520746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63accf6bcd464184eda8",
+    "url": "/react-project/static/js/main.428e744f.chunk.js"
+  },
+  {
+    "revision": "804c2dcac37ed42155f0",
+    "url": "/react-project/static/js/runtime-main.c80b379e.js"
+  }
+]);
